ae374ceb4b0a3e97d12d708941acb7db04024acb
Pquintan <pquintan@student.42barcelona.com>
2024-02-05 15:56:08 +0100

    command env done

--- minishell/srcs/main/builtins/env.c
+++ minishell/srcs/main/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: pquintan <pquintan@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 13:55:26 by pquintan          #+#    #+#             */
-/*   Updated: 2024/02/05 13:07:14 by pquintan         ###   ########.fr       */
+/*   Updated: 2024/02/05 15:55:23 by pquintan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,9 @@ void	ft_env(t_info *data)
 	t_info *temp;
 	
 	temp = data;
-	while (temp->list_env)
+	while (temp->list_env && temp->list_env->next)
 	{
 		printf("%s\n", temp->list_env->content);
 		temp->list_env = temp->list_env->next;
 	}
 }
-// Funcion prototipo
