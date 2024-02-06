9eea02468713d9bec084a20cc59354d843b49bfb
Pquintan <pquintan@student.42barcelona.com>
2024-02-06 16:10:19 +0100

    test commit

--- minishell/srcs/main/builtins/pwd.c
+++ minishell/srcs/main/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: pquintan <pquintan@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 13:55:26 by pquintan          #+#    #+#             */
-/*   Updated: 2024/02/05 17:32:28 by pquintan         ###   ########.fr       */
+/*   Updated: 2024/02/06 16:09:59 by pquintan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ void	ft_pwd(void)
 	buffer = malloc(sizeof (char)* 5000);
 	printf("%s\n", getcwd(buffer, 5000));
 	free(buffer);
+
 }
-// Funcion prototipo
